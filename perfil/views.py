6ab953e9e14091d2a12f6ceaeaf7bf6e59d63035
Joao Eurico <joao.eurico@eeg.tech>
2023-07-06 20:54:38 +0000

    getting data from form into view

--- perfil/views.py
+++ perfil/views.py
@@ -7,3 +7,15 @@ def home(request):
 
 def gerenciar(request):
     return render(request,'gerenciar.html')
+
+def cadastrar_banco(request):
+    # request.post will receive the values filled in on the form
+    # for each named field there is a key in the dictionary
+    # to access the fields value use the .get() method with the name of the field
+    apelido = request.POST.get('apelido')
+    banco = request.POST.get('banco')
+    tipo = request.POST.get('tipo')
+    valor = request.POST.get('valor')
+    icone = request.POST.get('icone')
+    print(request.POST)
+    return HttpResponse("Banco cadastrado")
