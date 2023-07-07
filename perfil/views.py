1de5a225b69008ec0e8e311ef8f83421bb789883
Joao Eurico <joao.eurico@eeg.tech>
2023-07-07 19:17:10 +0000

    added categorias actions (add)

--- perfil/views.py
+++ perfil/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.db.models import Sum
 from django.contrib.messages import constants
 
-from .models import Conta
+from .models import Conta, Categoria
 from django.http import HttpResponse
 
 # Create your views here.
@@ -15,6 +15,7 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
 
     # computing the sum on the client side
     # total_contas = 0
@@ -26,6 +27,7 @@ def gerenciar(request):
 
     return render(request, 'gerenciar.html', {
         'contas': contas,
+        'categorias': categorias,
         'total_contas': total_contas
     })
 
@@ -66,4 +68,33 @@ def cadastrar_banco(request):
     messages.add_message(request, constants.SUCCESS,
                          f"Conta criada com sucesso. Id={newConta.pk}")
 
+    return redirect('/perfil/gerenciar')
+
+
+def cadastrar_categoria(request):
+    # request.post will receive the values filled in on the form
+    # for each named field there is a key in the dictionary
+    # to access the fields value use the .get() method with the name of the field
+    categoria = request.POST.get('categoria').strip()
+    jatem = Categoria.objects.filter(categoria=categoria)
+    if len(jatem) > 0:
+        messages.add_message(request, constants.ERROR,
+                             'Categoria já existe')
+        return redirect('/perfil/gerenciar')
+    essencial = request.POST.get('essencial')
+    if type(essencial) == str:
+        essencial = True
+    else:
+        essencial = False
+
+    if len(categoria) == 0:
+        messages.add_message(request, constants.ERROR,
+                             'Categoria em branco não pode!')
+        return redirect('/perfil/gerenciar')
+
+    newCategoria = Categoria(categoria=categoria, essencial=essencial)
+    newCategoria.save()
+
+    messages.add_message(request, constants.SUCCESS,
+                         f"Categoria criada com sucesso. Id={newCategoria.id}")
     return redirect('/perfil/gerenciar')
