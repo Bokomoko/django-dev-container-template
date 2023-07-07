52036886de1e539c48818ad35e047060b4223582
Joao Eurico <joao.eurico@eeg.tech>
2023-07-07 18:44:12 +0000

    Delete account

--- perfil/views.py
+++ perfil/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib import messages
+from django.db.models import Sum
 from django.contrib.messages import constants
 
 from .models import Conta
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -14,14 +15,31 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
-    total_contas = 0
-    for conta in contas:
-        total_contas += float(conta.valor)
+
+    # computing the sum on the client side
+    # total_contas = 0
+    # f or conta in contas:
+    #    total_contas += float(conta.valor)
+
+    # computing the sum on the server side
+    total_contas = Conta.objects.aggregate(Sum('valor'))['valor__sum']
 
     return render(request, 'gerenciar.html', {
         'contas': contas,
         'total_contas': total_contas
     })
+
+
+def deletar_banco(request, id):
+    try:
+        Conta.objects.get(pk=id).delete()
+        messages.add_message(request, constants.SUCCESS,
+                             f"Conta deletada com sucesso.")
+    except:
+        messages.add_message(request, constants.ERROR,
+                             f"Não foi possível deletar a conta {id}.")
+
+    return redirect('/perfil/gerenciar')
 
 
 def cadastrar_banco(request):
